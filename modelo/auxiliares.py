63e45012fa1aec90f547e3cdfdb12d118655c606
Nicolas Rodriguez <115905475+Ahrenico@users.noreply.github.com>
2025-04-27 22:20:11 -0300

    Update auxiliares.py

--- modelo/auxiliares.py
+++ modelo/auxiliares.py
@@ -1,15 +1,16 @@
 import db
 #convierte el total de los pesos en un nivel-no va a servir para presentismo
 def obtener_nivel(x):
-    x = max(x, min(25)) #25 si el peso va de 1 a 5, 15 si va de 1 a 3
+    x = max(x, min(25)) #25 si el peso va de 1 a 5, 15 si va de 1 a 3 esto esta mal
     nivel = (x * 5) // 26 +1 #3 y 16 si el peso y el nivel cambian
     return int(nivel)
 
 #obtener habilidades, certificacion y presencia en proyectos, devuelve un numero del 1 al 5
 def obtener_nivel_habilidad(id_empleado,conexion):
     habilidades = db.realizar_consulta(conexion,"query para obtener pesos de habilidades de empleado %s", (id_empleado,))
-    valor_habilidades = sum(r[1] for r in habilidades) #obtiene el peso total, 1 seria la columna donde estan los pesos
-    nivel = obtener_nivel(valor_habilidades) #obtenemos el nivel para el modelo
+    #valor_habilidades = sum(r[1] for r in habilidades) #obtiene el peso total, 1 seria la columna donde estan los pesos
+    #nivel = obtener_nivel(valor_habilidades) #obtenemos el nivel para el modelo
+    nivel = obtener_nivel( habilidades [0])
     return nivel
 
 
